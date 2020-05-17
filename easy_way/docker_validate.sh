4c83d86993ce9eb6d2278249d3228e61c70cfe6b
Jason Cronquist <jcronq@gmail.com>
2020-05-17 20:50:51 +0000

    removing sudo access requirement from docker solution

--- easy_way/docker_validate.sh
+++ easy_way/docker_validate.sh
@@ -4,15 +4,11 @@ function pretty_echo {
     echo -e "\e[32m$1\e[0m"
 }
 
-[[ -d /mnt/ramdisk ]] || sudo mkdir -p /mnt/ramdisk
-pretty_echo "Testing disk speed"
-bash/ramdiskctl test
-bash/ramdiskctl test
 pretty_echo "Building docker image"
 docker build --tag jcronq/ramdisk .
 pretty_echo "Running docker image and testing ram-disk speed"
 docker run --mount type=tmpfs,destination=/mnt/ramdisk jcronq/ramdisk
-pretty_echo "The speed above should be much faster than the disk speed."
-pretty_echo "Removing ramdisk"
+pretty_echo "The speed above should be over 2GB/s if running in-memory"
+pretty_echo "Stopping docker container"
 docker ps | grep jcronq | cut -d" " -f -1 | xargs -I {} sh -c "docker stop {}"
 
